216e638c6cf9b6d894168e99393260cb818b4a39
Johnson Towoju <johnsontodimu@gmail.com>
2025-08-04 17:50:26 +0100

    load env based on node environment, add test env to test scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,13 @@ import { DatabaseModule } from './database/database.module';
 import { SecretModule } from './secret/secret.module';
 
 @Module({
-    imports: [ConfigModule.forRoot(), DatabaseModule, SecretModule],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`,
+        }),
+        DatabaseModule,
+        SecretModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
