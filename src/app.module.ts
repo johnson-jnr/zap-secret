f5fe8ecb7543b43784d3685c5f859b2848801f5f
Johnson Towoju <johnsontodimu@gmail.com>
2025-08-05 13:28:27 +0100

    feat: config nest to serve spa

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { SecretModule } from './secret/secret.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', '..', 'client', 'dist'),
+            exclude: ['/api/{*any}'],
+        }),
+
         ConfigModule.forRoot({
             envFilePath: `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`,
         }),
